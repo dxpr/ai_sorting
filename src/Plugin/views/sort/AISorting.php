7a96813540ac681c2688b4b29b3d54590e5c23a0
Jurriaan Roelofs <jur@dxpr.com>
2024-09-22 18:07:35 +0200

    WIP get correct total trials number for views display @todo * Store the total in a database table (config object too slow for machine learning) * Load the total in the query() function, using the old SUM of all nodes as fallback

--- src/Plugin/views/sort/AISorting.php
+++ src/Plugin/views/sort/AISorting.php
@@ -4,9 +4,11 @@ namespace Drupal\ai_sorting\Plugin\views\sort;
 
 use Drupal\views\Plugin\views\sort\SortPluginBase;
 use Drupal\views\ResultRow;
-use Drupal\ai_sorting\Service\UCB2Calculator;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\views\Views;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 /**
  * AI-based sorting plugin for Views.
@@ -14,13 +16,6 @@ use Drupal\views\Views;
  * @ViewsSort("ai_sorting")
  */
 class AISorting extends SortPluginBase {
-
-  /**
-   * The UCB2 calculator service.
-   *
-   * @var \Drupal\ai_sorting\Service\UCB2Calculator
-   */
-  protected $ucb2Calculator;
 
   /**
    * Constructs a new AISorting object.
@@ -31,12 +26,9 @@ class AISorting extends SortPluginBase {
    *   The plugin_id for the plugin instance.
    * @param mixed $plugin_definition
    *   The plugin implementation definition.
-   * @param \Drupal\ai_sorting\Service\UCB2Calculator $ucb2_calculator
-   *   The UCB2 calculator service.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, UCB2Calculator $ucb2_calculator) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->ucb2Calculator = $ucb2_calculator;
   }
 
   /**
@@ -46,9 +38,71 @@ class AISorting extends SortPluginBase {
     return new static(
       $configuration,
       $plugin_id,
-      $plugin_definition,
-      $container->get('ai_sorting.ucb2_calculator')
+      $plugin_definition
     );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $options['alpha'] = ['default' => 2];
+    // Remove the 'order' option
+    unset($options['order']);
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+    
+    // Remove the order selector
+    unset($form['order']);
+
+    $form['ucb1_settings'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('AI Sorting Settings'),
+    ];
+
+    $url = Url::fromUri('https://medium.com/analytics-vidhya/multi-armed-bandit-analysis-of-upper-confidence-bound-algorithm-4b84be516047', [
+      'attributes' => [
+        'target' => '_blank',
+        'rel' => 'noopener noreferrer',
+      ],
+    ]);
+    $link = Link::fromTextAndUrl($this->t('Learn more about the UCB algorithm'), $url);
+
+    $form['ucb1_settings']['alpha'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Exploration-Exploitation Balance'),
+      '#default_value' => $this->options['alpha'],
+      '#min' => 0,
+      '#max' => 10,
+      '#step' => 0.1,
+      '#description' => $this->t('Controls the balance between exploring new options and exploiting known successful options. Higher values encourage more exploration. Typical values range from 1 to 3. A lower value (closer to 0) will favor showing content that has performed well in the past. A higher value will encourage trying out more varied content. @link', [
+        '@link' => $link->toString(),
+      ]),
+      '#field_prefix' => $this->t('Alpha:'),
+      '#field_suffix' => $this->t('(0.0 to 10.0)'),
+      '#required' => TRUE,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::submitOptionsForm($form, $form_state);
+
+    $options = &$form_state->getValue('options');
+
+    // Save the alpha value
+    if (isset($options['ucb1_settings']['alpha'])) {
+      $this->options['alpha'] = $options['ucb1_settings']['alpha'];
+    }
   }
 
   /**
@@ -56,31 +110,37 @@ class AISorting extends SortPluginBase {
    */
   public function query() {
     $this->ensureMyTable();
-  
+
+    \Drupal::logger('ai_sorting')->notice('query() called');
+    // Log the view and display ID
+    \Drupal::logger('ai_sorting')->notice('AI Sorting query() called for view: @view_id, display: @display_id', [
+      '@view_id' => $this->view->id(),
+      '@view_id' => $this->view->id(),
+      '@display_id' => $this->view->current_display,
+    ]);
+
     // Calculate the total number of trials across all nodes.
-    // This is essential for the UCB1 exploration term.
-    // Using a subquery to sum ai_sorting_trials from node_counter.
-    // Note: Depending on the database size, this may impact performance.
     $totalTrialsSubquery = "(SELECT SUM(COALESCE(ai_sorting_trials, 1)) FROM node_counter)";
-  
-    // Define constants for the UCB1 algorithm.
-    // alpha: exploration parameter. Typically set to 2 for UCB1.
-    $alpha = 2;
-  
+
+    // Retrieve the alpha parameter from options.
+    $alpha = $this->options['alpha'];
+
     // Construct the UCB1 formula within the ORDER BY clause.
-    // UCB1 = (totalcount / ai_sorting_trials) + sqrt((alpha * ln(totalTrials)) / ai_sorting_trials)
     $ucb1Formula = "(COALESCE(node_counter.totalcount, 0) / GREATEST(COALESCE(node_counter.ai_sorting_trials, 1), 1)) + " .
                   "SQRT(($alpha * LN($totalTrialsSubquery)) / GREATEST(COALESCE(node_counter.ai_sorting_trials, 1), 1)) + " .
                   "(RAND() * 0.000001)";
-  
-    // Add the ORDER BY clause with the UCB1 formula.
+
+    // Log the UCB1 formula
+    \Drupal::logger('ai_sorting')->notice('UCB1 Formula: @formula', ['@formula' => $ucb1Formula]);
+
+    // Always use DESC order for UCB1 scores
     $this->query->addOrderBy(
       NULL,
       $ucb1Formula,
-      $this->options['order'],
+      'DESC',
       'node_ucb1_score'
     );
-  
+
     // Add the necessary JOIN to the node_counter table.
     $join = Views::pluginManager('join')->createInstance('standard', [
       'table' => 'node_counter',
@@ -91,4 +151,5 @@ class AISorting extends SortPluginBase {
     ]);
     $this->query->addRelationship('node_counter', $join, 'node_field_data');
   }
+
 }
